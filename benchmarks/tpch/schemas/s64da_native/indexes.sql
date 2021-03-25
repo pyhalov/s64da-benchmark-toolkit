fe5daec369191b4d7958f64afc9f7a33a8486bbf
Said Karimov <said.karimov@swarm64.com>
2021-03-25 21:49:38 +0100

    SDB-9856

--- benchmarks/tpch/schemas/s64da_native/indexes.sql
+++ benchmarks/tpch/schemas/s64da_native/indexes.sql
@@ -14,3 +14,65 @@ CREATE INDEX IDX_LINEITEM_SHIPDATE ON LINEITEM (L_SHIPDATE);
 CREATE INDEX IDX_LINEITEM_COMMITDATE ON LINEITEM (L_COMMITDATE);
 CREATE INDEX IDX_LINEITEM_RECEIPTDATE ON LINEITEM (L_RECEIPTDATE);
 CREATE INDEX IDX_ORDERS_ORDERDATE ON ORDERS (O_ORDERDATE);
+
+CREATE INDEX customer_columnstore_idx ON customer USING columnstore (
+    c_custkey,
+    c_name,
+    c_address,
+    c_nationkey,
+    c_phone,
+    c_acctbal,
+    c_mktsegment,
+    c_comment
+);
+
+CREATE INDEX orders_columnstore_idx ON orders USING columnstore (
+    o_orderkey,
+    o_custkey,
+    o_orderstatus,
+    o_totalprice,
+    o_orderdate,
+    o_orderpriority,
+    o_clerk,
+    o_shippriority,
+    o_comment
+);
+
+CREATE INDEX part_columnstore_idx ON part USING columnstore (
+    p_partkey,
+    p_name,
+    p_mfgr,
+    p_brand,
+    p_type,
+    p_size,
+    p_container,
+    p_retailprice,
+    p_comment
+);
+
+CREATE INDEX partsupp_columnstore_idx ON partsupp USING columnstore (
+    ps_partkey,
+    ps_suppkey,
+    ps_availqty,
+    ps_supplycost,
+    ps_comment
+);
+
+CREATE INDEX lineitem_columnstore_idx ON lineitem USING columnstore (
+    l_orderkey,
+    l_partkey,
+    l_suppkey,
+    l_linenumber,
+    l_quantity,
+    l_extendedprice,
+    l_discount,
+    l_tax,
+    l_returnflag,
+    l_linestatus,
+    l_shipdate,
+    l_commitdate,
+    l_receiptdate,
+    l_shipinstruct,
+    l_shipmode,
+    l_comment
+);
