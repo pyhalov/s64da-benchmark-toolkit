13ed30cb37a7073335d6e0632543cc87af2781c7
Spiros N. Agathos <spiros@swarm64.com>
2021-07-12 18:44:30 +0300

    added multiple brin instead of csi

--- benchmarks/htap/schemas/s64da_native_enhanced/s64da_indexes.sql
+++ benchmarks/htap/schemas/s64da_native_enhanced/s64da_indexes.sql
@@ -1,41 +1,33 @@
-CREATE INDEX idx_customer_cache ON customer USING columnstore (
-    c_id
-  , c_d_id
-  , c_w_id
-  , c_nationkey
-  , c_last
-  , c_city
-  , c_phone
-  , c_state
-  , c_balance
-);
+CREATE INDEX idx_customer_cache1 ON customer USING BRIN (c_id);
+CREATE INDEX idx_customer_cache2 ON customer USING BRIN (c_d_id);
+CREATE INDEX idx_customer_cache3 ON customer USING BRIN (c_w_id);
+CREATE INDEX idx_customer_cache4 ON customer USING BRIN (c_nationkey);
+CREATE INDEX idx_customer_cache5 ON customer USING BRIN (c_last);
+CREATE INDEX idx_customer_cache6 ON customer USING BRIN (c_city);
+CREATE INDEX idx_customer_cache7 ON customer USING BRIN (c_phone);
+CREATE INDEX idx_customer_cache8 ON customer USING BRIN (c_state);
+CREATE INDEX idx_customer_cache9 ON customer USING BRIN (c_balance);
 
-CREATE INDEX idx_orders_cache ON orders USING columnstore (
-    o_id
-  , o_d_id
-  , o_w_id
-  , o_c_id
-  , o_entry_d
-  , o_carrier_id
-  , o_ol_cnt
-  , o_all_local
-);
+CREATE INDEX idx_orders_cache1 ON orders USING BRIN (o_id);
+CREATE INDEX idx_orders_cache2 ON orders USING BRIN (o_d_id);
+CREATE INDEX idx_orders_cache3 ON orders USING BRIN (o_w_id);
+CREATE INDEX idx_orders_cache4 ON orders USING BRIN (o_c_id);
+CREATE INDEX idx_orders_cache5 ON orders USING BRIN (o_entry_d);
+CREATE INDEX idx_orders_cache6 ON orders USING BRIN (o_carrier_id);
+CREATE INDEX idx_orders_cache7 ON orders USING BRIN (o_ol_cnt);
+CREATE INDEX idx_orders_cache8 ON orders USING BRIN (o_all_local);
 
-CREATE INDEX idx_order_line_cache ON order_line USING columnstore (
-    ol_o_id
-  , ol_d_id
-  , ol_w_id
-  , ol_number
-  , ol_i_id
-  , ol_supply_w_id
-  , ol_delivery_d
-  , ol_quantity
-  , ol_amount
-);
+CREATE INDEX idx_order_line_cache1 ON order_line USING BRIN (ol_o_id);
+CREATE INDEX idx_order_line_cache2 ON order_line USING BRIN (ol_d_id);
+CREATE INDEX idx_order_line_cache3 ON order_line USING BRIN (ol_w_id);
+CREATE INDEX idx_order_line_cache4 ON order_line USING BRIN (ol_number);
+CREATE INDEX idx_order_line_cache5 ON order_line USING BRIN (ol_i_id);
+CREATE INDEX idx_order_line_cache6 ON order_line USING BRIN (ol_supply_w_id);
+CREATE INDEX idx_order_line_cache7 ON order_line USING BRIN (ol_delivery_d);
+CREATE INDEX idx_order_line_cache8 ON order_line USING BRIN (ol_quantity);
+CREATE INDEX idx_order_line_cache9 ON order_line USING BRIN (ol_amount);
 
-CREATE INDEX idx_stock_cache ON stock USING columnstore (
-    s_i_id
-  , s_w_id
-  , s_quantity
-  , s_order_cnt
-);
+CREATE INDEX idx_stock_cache1 ON stock USING BRIN (s_i_id);
+CREATE INDEX idx_stock_cache2 ON stock USING BRIN (s_w_id);
+CREATE INDEX idx_stock_cache3 ON stock USING BRIN (s_quantity);
+CREATE INDEX idx_stock_cache4 ON stock USING BRIN (s_order_cnt);
