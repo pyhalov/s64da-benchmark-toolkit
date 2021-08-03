dd75388bb4524612aa7dea75297e16364e5e4c3d
Spiros N. Agathos <spiros@swarm64.com>
2021-08-03 17:58:46 +0300

    Use only 4 cols per csi

--- benchmarks/htap/schemas/s64da_native_enhanced/s64da_indexes.sql
+++ benchmarks/htap/schemas/s64da_native_enhanced/s64da_indexes.sql
@@ -3,11 +3,6 @@ CREATE INDEX idx_customer_cache ON customer USING columnstore (
   , c_d_id
   , c_w_id
   , c_nationkey
-  , c_last
-  , c_city
-  , c_phone
-  , c_state
-  , c_balance
 );
 
 CREATE INDEX idx_orders_cache ON orders USING columnstore (
@@ -15,10 +10,6 @@ CREATE INDEX idx_orders_cache ON orders USING columnstore (
   , o_d_id
   , o_w_id
   , o_c_id
-  , o_entry_d
-  , o_carrier_id
-  , o_ol_cnt
-  , o_all_local
 );
 
 CREATE INDEX idx_order_line_cache ON order_line USING columnstore (
@@ -26,11 +17,6 @@ CREATE INDEX idx_order_line_cache ON order_line USING columnstore (
   , ol_d_id
   , ol_w_id
   , ol_number
-  , ol_i_id
-  , ol_supply_w_id
-  , ol_delivery_d
-  , ol_quantity
-  , ol_amount
 );
 
 CREATE INDEX idx_stock_cache ON stock USING columnstore (
