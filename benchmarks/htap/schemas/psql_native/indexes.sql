38e7eb27f4ba2cf99443734983a39a98596b8719
Spiros N. Agathos <spiros@swarm64.com>
2021-08-03 17:41:11 +0300

    Added 4 bogus indexes in psql native schema

--- benchmarks/htap/schemas/psql_native/indexes.sql
+++ benchmarks/htap/schemas/psql_native/indexes.sql
@@ -15,3 +15,19 @@ CREATE INDEX fkey_order_line ON order_line (
     ol_supply_w_id
   , ol_i_id
 );
+
+CREATE INDEX idx_customer_bogus ON customer (
+    c_payment_cnt
+);
+
+CREATE INDEX idx_orders_bogus ON orders (
+    o_all_local
+);
+
+CREATE INDEX idx_order_line_bogus ON order_line (
+    ol_amount
+);
+
+CREATE INDEX idx_stock_bogus ON stock (
+    s_ytd
+);
